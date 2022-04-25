05e0d66793640c213e063cd821b803031c78a0c9
CharlieCernov <100284435+CharlieCernov@users.noreply.github.com>
2022-04-25 09:22:57 -0300

    Add files via upload

---
+++ javascript/json.js
@@ -0,0 +1,19 @@
+function NaoMeClique(){
+    prompt("Mãos ao alvo!");
+}
+
+function focusMe(){
+    console.log("fui focado!");
+}
+
+function blurMe(){
+    console.log("fui desfocado!");
+}
+
+function changeMe(){
+    console.log("Tem uma cobra na minha bota!");
+}
+
+function overMe(){
+    console.log("Amigo estou aqui!");
+}
